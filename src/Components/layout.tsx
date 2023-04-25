11dcf0f2090a658338961b4d34361d6111a76e84
zacharyad <zacharyadcoding@gmail.com>
2023-04-25 10:12:02 -0400

    Made a home and profile when sighned in topright nav

--- src/Components/layout.tsx
+++ src/Components/layout.tsx
@@ -1,9 +1,24 @@
 import type { PropsWithChildren } from "react";
+import Link from "next/link";
+import { useUser } from "@clerk/nextjs";
 
 export const PageLayout = (props: PropsWithChildren) => {
+  const { user } = useUser();
+  // eagerly running this so it is cached and able to be used in the Feed Component
+
   return (
-    <main className="overflow-none flex h-screen justify-center">
+    <main className="overflow-none relative flex h-screen justify-center">
       <div className="flex h-full w-full flex-col border-x border-slate-400 md:max-w-2xl">
+        <div className="absolute right-12 top-4 z-10 flex flex-col justify-between gap-6">
+          <Link href="/">
+            <button>Feed</button>
+          </Link>
+          {user && (
+            <Link href={`/@${user.username}`}>
+              <button>Profile</button>
+            </Link>
+          )}
+        </div>
         {props.children}
       </div>
     </main>
